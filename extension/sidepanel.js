8739b8d004f9440c5d4bac6b4dbb99aa28fd1e9e
Hemanth Chebiyam <hc3746@rit.edu>
2025-04-21 18:12:56 -0400

    amplify push

--- extension/sidepanel.js
+++ extension/sidepanel.js
@@ -1,8 +1,8 @@
 // API Configuration
-const API_ENDPOINT = 'https://y92m42xlij.execute-api.us-east-1.amazonaws.com/prod';
+const API_ENDPOINT = 'https://w7jhwtbzg2.execute-api.us-east-1.amazonaws.com/prod';
 const AUTH_ENDPOINT = `${API_ENDPOINT}/auth`;
 const SUMMARIZE_ENDPOINT = `${API_ENDPOINT}/summarize`;
-const COGNITO_CLIENT_ID = '6e4dkc1t8ninc5kcomfimjkbvq';
+const COGNITO_CLIENT_ID = '2cjrkd09sl4mr1uubs33kn1o0s';
 
 // UI Elements
 const authContainer = document.getElementById('authContainer');
@@ -891,4 +891,6 @@ async function fetchHistory() {
         historyLoading.style.display = 'none';
     }
 }
+
+
 
